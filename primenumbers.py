9eac680dd964a34cef9cebda8db6e78e88969ce2
ianoti <ianodhiambootieno@gmail.com>
2016-10-28 17:47:03 +0300

    version of generator with bug

--- primenumbers.py
+++ primenumbers.py
@@ -1,3 +1,31 @@
-def primegenerator(n):
-	pass
-	
+# def primegenerator(number): #generate prime number
+# 	prime = [2]
+# 	check = 3
+# 	while True:
+# 		for i in range (2, int(check**0.5)+1):
+# 			if number % i == 0 or number % 2 ==0:
+# 				break
+# 		else:
+# 			prime.append(check)
+# 		check = check + 1
+# 	if check == number:
+# 		break
+# 	return prime
+
+# print (primegenerator(80))
+def primegenerator(number):
+	prime = [2]
+	check = 3
+	while True:
+		for i in range (2, int(check**0.5)+1):
+			if number % i == 0 or number % 2 ==0:
+				pass
+			else:
+				prime.append(check)
+			check = check + 1
+		if check == number:
+			break
+
+	return prime
+
+print (primegenerator(80))
