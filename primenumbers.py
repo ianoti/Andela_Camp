3a781d6d58074ca53c43f7d138ca866589d3d0b2
ianoti <ianodhiambootieno@gmail.com>
2016-10-28 18:10:01 +0300

    The nonresponsive bug has been fixed and a new bug of only printing 2 discovered

--- primenumbers.py
+++ primenumbers.py
@@ -1,31 +1,15 @@
-# def primegenerator(number): #generate prime number
-# 	prime = [2]
-# 	check = 3
-# 	while True:
-# 		for i in range (2, int(check**0.5)+1):
-# 			if number % i == 0 or number % 2 ==0:
-# 				break
-# 		else:
-# 			prime.append(check)
-# 		check = check + 1
-# 	if check == number:
-# 		break
-# 	return prime
-
-# print (primegenerator(80))
 def primegenerator(number):
 	prime = [2]
 	check = 3
 	while True:
-		for i in range (2, int(check**0.5)+1):
+		for i in range (2, number+1):
 			if number % i == 0 or number % 2 ==0:
-				pass
+				break
 			else:
 				prime.append(check)
 			check = check + 1
 		if check == number:
 			break
+		return prime
 
-	return prime
-
-print (primegenerator(80))
+print (primegenerator(70))
