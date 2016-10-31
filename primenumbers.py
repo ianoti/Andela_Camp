265e070eb1bdb0a5d74696bb8b20980e1a7eb8f9
ianoti <ianodhiambootieno@gmail.com>
2016-10-31 11:40:40 +0300

    code edited to remove excess comments and extra print command

--- primenumbers.py
+++ primenumbers.py
@@ -1,26 +1,3 @@
-# def primegenerator(number):
-# 	prime = [2]
-# 	check = 3
-# 	while True:
-# 		for i in range (3, int(check**0.5)+1):
-# 			if check % i == 0 or check % 2 ==0:
-# 				break
-# 		else:
-# 			prime.append(check)
-# 			check = check + 1
-# 		if check == number:
-# 			break
-# 	return prime
-
-# print (primegenerator(70))
-# num = 80
-# for i in range(2,num+1):
-#     count = 0
-#     for j in range(2,i):
-#         if i%j != 0:
-#             count += 1
-#     if count == i-2:
-#         print (i)
 def prime(num):
 	for i in range(2,num+1):
 		count = 0
@@ -29,5 +6,3 @@ def prime(num):
 				count += 1
 		if count == i-2: #this is due to starting from 2 in the primes
 			print (i)
-
-print (prime(70))
